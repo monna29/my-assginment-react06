6cc7bb9c28f46b021d8435b8b6e2b27822b1dd72
monna29 <monnamia255@gmail.com>
2026-04-02 17:35:53 +0600

    Pricing color add

--- src/componemts/Pricing/Pricing.jsx
+++ src/componemts/Pricing/Pricing.jsx
@@ -22,19 +22,19 @@ class Pricing extends Component {
                         <button className='btn rounded-full text-white w-full bg-gradient-to-r from-[#4f39f6] to-[#9514fa]'>Get Started Free</button>
                     </div>
 
-                     <div className='inset-shadow-sm bg-[#E1E7FF]  rounded-2xl p-4 '>
-                        <h3 className='text-2xl font-medium mb-2'> Pro</h3>
-                        <p> Best for professionals</p>
-                        <p className='my-6'><span className='text-3xl font-medium'>$29</span>/month</p>
+                     <div className='inset-shadow-sm bg-gradient-to-r from-[#4f39f6] to-[#9514fa]  rounded-2xl p-4 '>
+                        <h3 className='text-2xl text-white font-medium mb-2'> Pro</h3>
+                        <p className='text-white'> Best for professionals</p>
+                        <p className='my-6 text-white'><span className='text-3xl font-medium'>$29</span>/month</p>
                         <div className='h-40 space-y-2'>
-                            <p className='flex gap-1'> <Check className='text-[#30B868]' /> Basic templates</p>
-                            <p className='flex gap-1'> <Check className='text-[#30B868]' /> Community support</p>
-                            <p className='flex gap-1'> <Check className='text-[#30B868]' /> 1 project per month</p>
-                            <p className='flex gap-1'> <Check className='text-[#30B868]' /> Access to 10 free tools</p>
-                            <p className='flex gap-1'> <Check className='text-[#30B868]' /> Access to 10 free tools</p>
-                            <p className='flex gap-1'> <Check className='text-[#30B868]' /> Access to 10 free tools</p>
+                            <p className='flex gap-1 text-white'> <Check className='text-[#30B868]' /> Basic templates</p>
+                            <p className='flex gap-1 text-white'> <Check className='text-[#30B868]' /> Community support</p>
+                            <p className='flex gap-1 text-white'> <Check className='text-[#30B868]' /> 1 project per month</p>
+                            <p className='flex gap-1 text-white'> <Check className='text-[#30B868]' /> Access to 10 free tools</p>
+                            <p className='flex gap-1 text-white'> <Check className='text-[#30B868]' /> Access to 10 free tools</p>
+                            <p className='flex gap-1 text-white'> <Check className='text-[#30B868]' /> Access to 10 free tools</p>
                         </div>
-                        <button className='btn rounded-full text-white w-full bg-gradient-to-r from-[#4f39f6] to-[#9514fa]'>Get Started Free</button>
+                        <button className='btn rounded-full  w-full bg-white'>Get Started Free</button>
                     </div>
 
                     
